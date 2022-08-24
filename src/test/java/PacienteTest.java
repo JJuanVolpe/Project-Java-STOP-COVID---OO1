bb485c191d871a803b5cf41d5adda3f9e524ad45
juan <juan.voolpe@gmail.com>
2022-08-24 13:11:51 -0300

    Se agregó caso de prueba en caso de necesitar hacer un control pcr si hubo un clinico positivo (permitiendo hacerlo en la misma semana)

--- src/test/java/PacienteTest.java
+++ src/test/java/PacienteTest.java
@@ -2,7 +2,6 @@ import classes.ControlClinico;
 import classes.ControlLaboratorio;
 import classes.Paciente;
 import classes.ControlPcr;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.LocalDate;
@@ -21,11 +20,22 @@ public class PacienteTest {
         p.agregarControl(new ControlClinico(LocalDate.now().plusWeeks(1), true));
         assertFalse(p.noTieneControlesPositivos());
     }
+    @Test
+    public void realizarControlPcr(){
+        Paciente p = new Paciente("Jose", "M", "12", 20, "0019");
+        p.agregarControl(new ControlClinico(LocalDate.now(), true));
+        assertEquals(1, p.getControles().size());
+
+        p.agregarControl(new ControlClinico(LocalDate.now(), true));
+        assertEquals(1, p.getControles().size());   //No se agrega por el tiempo y tipo del mismo
+        p.agregarControl(new ControlPcr(LocalDate.now(), true));
+        assertEquals(2, p.getControles().size());   //Si se agrega, por ser pcr y tener cómo último un estudio clínico positivo
+    }
 
     @Test
     public void getControlsByWeeks(){
         Paciente p = new Paciente("Jose", "M", "12", 20, "0019");
-        addLabControls(p);
+        addSevenLabControls(p);
         assertEquals(3, p.getControlsLabByTime(true).size());
         assertEquals(6, p.getControlsLabByTime(false).size());
     }
@@ -33,29 +43,31 @@ public class PacienteTest {
     @Test
     public void hasControlesPositivos(){
         Paciente p = new Paciente("Jose", "M", "12", 20, "0019");
-        addLabControls(p);
+        addSevenLabControls(p);
         p.agregarControl(new ControlClinico(LocalDate.now().plusWeeks(0), false));
         assertTrue(p.noTieneControlesPositivos());
-        p.agregarControl(new ControlClinico(LocalDate.now().plusWeeks(6), true));
+        p.agregarControl(new ControlClinico(LocalDate.now().plusWeeks(7), true));
         assertFalse(p.noTieneControlesPositivos());
     }
 
     @Test
     public void booleanReturnByControls(){
         Paciente p = new Paciente("Jose", "M", "12", 20, "0019");
-        addLabControls(p);
-        p.agregarControl(new ControlPcr(LocalDate.now().plusWeeks(6), true));
+        addSevenLabControls(p);
+        p.agregarControl(new ControlPcr(LocalDate.now().plusWeeks(7), true));
         assertTrue(p.poseePcrPositivo());
         assertFalse(p.poseeClinicoPositivo());
         assertTrue(p.poseeControlLaboratorio());
     }
 
-    private void addLabControls(Paciente p) {
+    private void addSevenLabControls(Paciente p) {
         p.agregarControl(new ControlLaboratorio(LocalDate.now(),0d));
         p.agregarControl(new ControlLaboratorio(LocalDate.now().plusWeeks(1),10d));
         p.agregarControl(new ControlLaboratorio(LocalDate.now().plusWeeks(2),20d));
         p.agregarControl(new ControlLaboratorio(LocalDate.now().plusWeeks(3),30d));
         p.agregarControl(new ControlLaboratorio(LocalDate.now().plusWeeks(4),40d));
         p.agregarControl(new ControlLaboratorio(LocalDate.now().plusWeeks(5),60d));
+        p.agregarControl(new ControlLaboratorio(LocalDate.now().plusWeeks(6),70d));
+
     }
 }
